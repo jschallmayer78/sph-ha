79506b3d1f6bd3adf37b79d9cfc947b9bf9950f6
Leonid Kogan <leonsio@users.noreply.github.com>
2026-08-15 22:47:25 +0200

    Remove class attribute from timetable sensor

--- custom_components/sph_stundenplan/sensor.py
+++ custom_components/sph_stundenplan/sensor.py
@@ -1,8 +1,11 @@
 from homeassistant.components.sensor import SensorEntity
-from .const import DOMAIN, CONF_CLASS
+
+from .const import DOMAIN
+
 
 async def async_setup_entry(hass, entry, async_add_entities):
     async_add_entities([SphTimetableSensor(hass.data[DOMAIN][entry.entry_id], entry)])
+
 
 class SphTimetableSensor(SensorEntity):
     _attr_has_entity_name = True
@@ -21,7 +24,11 @@ class SphTimetableSensor(SensorEntity):
     @property
     def extra_state_attributes(self):
         data = self.coordinator.data or {}
-        return {"klasse": self.entry.data.get(CONF_CLASS, ""), "wochenkennung": data.get("week_badge"), "tage": data.get("all", []), "eigener_plan": data.get("own", [])}
+        return {
+            "wochenkennung": data.get("week_badge"),
+            "tage": data.get("all", []),
+            "eigener_plan": data.get("own", []),
+        }
 
     async def async_update(self):
         await self.coordinator.async_request_refresh()
