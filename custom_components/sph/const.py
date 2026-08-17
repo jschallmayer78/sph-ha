a74716c21fd52ae7ddb9c684d8649eec38045eba
Leonid Kogan <leonsio@users.noreply.github.com>
2026-08-17 11:07:14 +0200

    Set default SPH update interval to 60 minutes

--- custom_components/sph/const.py
+++ custom_components/sph/const.py
@@ -5,7 +5,7 @@ CONF_PASSWORD = "password"
 CONF_CHILD_NAME = "child_name"
 CONF_CHILD_SHORTCUT = "child_shortcut"
 CONF_UPDATE_INTERVAL = "update_interval"
-DEFAULT_UPDATE_INTERVAL = 15
+DEFAULT_UPDATE_INTERVAL = 60
 
 # Authentication is hosted separately from the legacy school portal.
 SPH_BASE = "https://start.schulportal.hessen.de"
