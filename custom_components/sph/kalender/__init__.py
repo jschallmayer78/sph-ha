67c120013fb14dd3e766b3b1db0100bdb03da19f
Leonid Kogan <leonsio@users.noreply.github.com>
2026-08-18 21:42:53 +0200

    refactor: remove old calendar module location

--- custom_components/sph/kalender/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Schulportal Hessen calendar module."""
-
-from .client import SphCalendarClient
-from .coordinator import SphCalendarCoordinator
-from .sensor import SphCalendarSensor
-
-__all__ = ["SphCalendarClient", "SphCalendarCoordinator", "SphCalendarSensor"]
