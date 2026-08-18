405a27cb2f67409a4413efbfad66fe3c85bfd536
Leonid Kogan <leonsio@users.noreply.github.com>
2026-08-18 21:43:24 +0200

    refactor: remove old timetable module location

--- custom_components/sph/stundenplan/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Schulportal Hessen timetable module."""
-
-from .client import SphTimetableClient
-from .coordinator import SphTimetableCoordinator
-from .sensor import SphTimetableSensor
-
-__all__ = ["SphTimetableClient", "SphTimetableCoordinator", "SphTimetableSensor"]
